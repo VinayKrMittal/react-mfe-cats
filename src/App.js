e81ab383f30c2c27217e869f39465b0e57508a3a
vinay.kumar1 <vinay.kumar1@telekom-digital.com>
2021-09-20 10:46:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import GreetingCat from "./routes/GreetingCat";
+import RandomCat from "./routes/RandomCat";
+import "./App.css";
+import CatGif from "./routes/CatGif";
 
-function App() {
+const defaultHistory = createBrowserHistory();
+
+function App({ history = defaultHistory }) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Route exact path="/" component={RandomCat} />
+      <Route exact path="/cat/:greeting" component={GreetingCat} />
+      <Route exact path="/gif" component={CatGif} />
+    </Router>
   );
 }
 
